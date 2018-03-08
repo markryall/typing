b9a0dc78c716eb70001b5ceb9ede741485cb2d04
Mark Ryall <mark@ryall.name>
2018-03-08 20:36:14 +1000

    extract script

---
+++ script.js
@@ -0,0 +1,106 @@
+const sentences = [
+	`When Gregor was already sticking half way out of the bed - the new
+method was more of a game than an effort, all he had to do was rock
+back and forth - it occurred to him how simple everything would be
+if somebody came to help him.`,
+	`Two strong people - he had his father
+and the maid in mind - would have been more than enough; they would
+only have to push their arms under the dome of his back, peel him
+away from the bed, bend down with the load and then be patient and
+careful as he swang over onto the floor, where, hopefully, the
+little legs would find a use.`,
+	`Should he really call for help
+though, even apart from the fact that all the doors were locked?
+Despite all the difficulty he was in, he could not suppress a smile
+at this thought.`,
+	`After a while he had already moved so far across that it would have
+been hard for him to keep his balance if he rocked too hard.`,
+	`The
+time was now ten past seven and he would have to make a final
+decision very soon.`,
+	`Then there was a ring at the door of the flat.
+"That'll be someone from work", he said to himself, and froze very
+still, although his little legs only became all the more lively as
+they danced around.`,
+	`For a moment everything remained quiet.
+"They're not opening the door", Gregor said to himself, caught in
+some nonsensical hope.`
+];
+
+function ts() {
+	return (new Date()).getTime();
+}
+
+const startTime = ts();
+let currentTime = startTime;
+const progress = [];
+const events = [];
+const corpus = sentences[ Math.floor( Math.random() * sentences.length ) ];
+
+const contentElement = document.getElementById( 'content' );
+const resultElement = document.getElementById( 'result' );
+
+function renderProgress() {
+	let content = '';
+
+	progress.forEach( function( key ) {
+		if ( key.expected ) {
+			const style = key.expected === key.actual ? 'right' : 'wrong';
+			content += `<span class="${style}">${key.expected}</span>`;
+		}
+	});
+
+	for( let index=progress.length; index < corpus.length; index++ ) {
+		content += corpus[index];
+	}
+
+	contentElement.innerHTML = content;
+}
+
+function renderResult( endTime ) {
+	let content = '';
+	const total = progress.length;
+	let correct = 0;
+	const seconds = ( endTime - startTime ) / 1000;
+
+	content += `<div>Congratulations! You finished in ${seconds} seconds</div>`;
+	content += `<div>You typed ${total} characters`;
+
+	progress.forEach( function( key ) {
+		if ( key.expected === key.actual) {
+			correct += 1;
+		}
+	} );
+
+	const percent = correct / total * 100;
+
+	content += `<div>${correct} were correct (${percent} %)`;
+
+	resultElement.innerHTML = content;
+}
+
+renderProgress();
+
+document.onkeypress = function ( event ) {
+	const newTime = ts();
+	if (event.key.length === 1) {
+		const expected = corpus[progress.length];
+		const actual = event.key;
+		const delay = newTime - currentTime;
+
+		progress.push({ expected, actual, delay });
+		renderProgress();
+		currentTime = newTime;
+		event.preventDefault();
+		if ( progress.length == corpus.length ) {
+			renderResult( newTime );
+		}
+	}
+}
+
+document.onkeyup = function( event ) {
+	if (event.code === 'Backspace') {
+		progress.splice(-1, 1);
+		renderProgress();
+	}
+}
